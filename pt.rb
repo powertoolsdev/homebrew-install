d5d5e993cc8039931c6f546af364f8088e8ed95b
Jon Morehouse <morehousej09@gmail.com>
2020-11-25 08:32:54 -0800

    pt: bump version

--- pt.rb
+++ pt.rb
@@ -1,12 +1,12 @@
 class Pt < Formula
   desc "Build, deploy and operate serverless,static and container apps with a single command."
   homepage "https://www.powertools.dev"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://download.powertools.dev/download/46ED852D-EC8F-4877-80E8-0248B7383912?version=9b7658f0805910b880f843b88759fe6d6fa24894"
-    sha256 "b5e72c3ff2ea20d3f06273a0b786a88c4395c44c3a43dee7deac8ff4eac0abc0"
+    url "https://download.powertools.dev/download/46ED852D-EC8F-4877-80E8-0248B7383912?version=16afc6cac7c46480533cae683a2dd9838957fc44"
+    sha256 "861ca54da8496c72deebd7d130140ac5d01773335e62c9bed244529603f937b3"
   end
 
   def install
