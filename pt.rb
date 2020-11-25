be83e4f2f8efc823cf1f885110d45b5afbddea3c
Jon Morehouse <morehousej09@gmail.com>
2020-11-24 23:06:57 -0800

    pt: fix pt download url

--- pt.rb
+++ pt.rb
@@ -5,7 +5,7 @@ class Pt < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://download.powertools.dev/46ED852D-EC8F-4877-80E8-0248B7383912?version=9b7658f0805910b880f843b88759fe6d6fa24894"
+    url "https://download.powertools.dev/download/46ED852D-EC8F-4877-80E8-0248B7383912?version=9b7658f0805910b880f843b88759fe6d6fa24894"
     sha256 "b5e72c3ff2ea20d3f06273a0b786a88c4395c44c3a43dee7deac8ff4eac0abc0"
   end
 
