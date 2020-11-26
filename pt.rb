460e754a72f2dd36dcf0068dfa8f946c65b2ed7d
Jon Morehouse <morehousej09@gmail.com>
2020-11-26 15:11:08 -0800

    pt: v0.0.7

--- pt.rb
+++ pt.rb
@@ -1,12 +1,12 @@
 class Pt < Formula
   desc "Build, deploy and operate serverless,static and container apps with a single command."
   homepage "https://www.powertools.dev"
-  version "0.0.6"
+  version "0.0.7"
   bottle :unneeded
 
   if OS.mac?
-    url "https://download.powertools.dev/download/46ED852D-EC8F-4877-80E8-0248B7383912?version=3873c735db845947cd12ad4692cdc91f56edef0e"
-    sha256 "c9f2b0d908ffbc61ceca55f04a37fd84c2c7ad7d939ec76ad3ef350ba7cf12b8"
+    url "https://download.powertools.dev/download/46ED852D-EC8F-4877-80E8-0248B7383912?version=488ff3c4ef3348184f5d5bdf5789c1635a6ce6db"
+    sha256 "ad8c7cd8ab30772387d239fbf0d36266ca52696957587005c56f41f4dff71147"
   end
 
   def install
